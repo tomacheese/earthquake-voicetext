a10091f07d6ce942e76aef629fea182c892966bb
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-03-31 07:11:56 +0900

    fix: error handling

--- src/main.ts
+++ src/main.ts
@@ -86,6 +86,10 @@ async function main(): Promise<void> {
   const response = await Axios.get(
     `http://www.kmoni.bosai.go.jp/webservice/hypo/eew/${datetime}.json`
   )
+  if (response.status !== 200) {
+    console.error('Failed to get earthquake data')
+    return
+  }
   console.log(date, JSON.stringify(response.data))
 
   const data = response.data
